d067db24843bb143f3f4bf49ab1b387d865bcc4d
fibo <casati_gianluca@yahoo.it>
2018-12-30 15:17:50 +0100

    added guitar tuner

--- docs/guitar-tuner-432/app.js
+++ docs/guitar-tuner-432/app.js
@@ -5,7 +5,39 @@ window.addEventListener('load', function () {
 
   // Golden ratio frequencies for guitar strings taken from here:
   // http://andreamaicreative.com/how-to-tune-your-guitar-to-432-hz/
-  const frequencies = [323, 242, 193, 144, 108, 81]
+  // Lower frequencies are played with a lower volume so it is necessary to adjust gain.
+  const notes = [
+    {
+      notation: 'E4',
+      frequency: 323,
+      gain: 0.2
+    },
+    {
+      notation: 'B3',
+      frequency: 242,
+      gain: 0.3
+    },
+    {
+      notation: 'G3',
+      frequency: 193,
+      gain: 0.5
+    },
+    {
+      notation: 'D3',
+      frequency: 144,
+      gain: 0.7
+    },
+    {
+      notation: 'A2',
+      frequency: 108,
+      gain: 0.9
+    },
+    {
+      notation: 'E2',
+      frequency: 81,
+      gain: 1.1
+    }
+  ]
 
   let playing = false
   const duration = 4
@@ -17,18 +49,33 @@ window.addEventListener('load', function () {
       } else {
         playing = true
 
+        buttons.forEach((button, j) => {
+          if (i !== j) {
+            button.disabled = true
+          }
+        })
+
         setTimeout(() => {
           playing = false
+
+          buttons.forEach(button => {
+            button.disabled = false
+          })
         }, duration * 1000)
       }
+
       const oscillator = context.createOscillator()
-      oscillator.frequency.value = frequencies[i]
-      oscillator.type = 'sine'
+      oscillator.frequency.value = notes[i].frequency
+      oscillator.type = 'triangle'
 
       const gainNode = context.createGain()
-      gainNode.gain.value = .5
+      gainNode.gain.value = notes[i].gain
 
-      oscillator.connect(context.destination)
+      // Fade out.
+      gainNode.gain.exponentialRampToValueAtTime(0.01, context.currentTime + duration);
+
+      oscillator.connect(gainNode)
+      gainNode.connect(context.destination)
 
       oscillator.start()
       oscillator.stop(context.currentTime + duration)
